fb5df9ecc39167db6474716f5ba58f24856f1c1d
Ouroboros42 <lucas.gathercole@gmail.com>
2025-04-02 18:54:18 +0100

    Add sets of parities

--- src/Hamiltonian/Util/Parity.jl
+++ src/Hamiltonian/Util/Parity.jl
@@ -1,6 +1,6 @@
-export Parity, MaybeParity, Even, Odd, AllParities
+export Parity, MaybeParity, Even, Odd
 
-import Base: range
+import Base: range, Set
 
 @enum Parity::UInt8 begin
     Even = 0x0
@@ -9,7 +9,11 @@ end
 
 const MaybeParity = Union{Parity, Nothing}
 
-const AllParities = Set{MaybeParity}([nothing, Even, Odd])
+const ALL_DEFINITE_PARITIES = Set{Parity}([Even, Odd])
+const ALL_PARITIES = Set{MaybeParity}([nothing, Even, Odd])
+
+Set(::Type{Parity}) = ALL_DEFINITE_PARITIES
+Set(::Type{MaybeParity}) = ALL_PARITIES
 
 number_parity(n::Unsigned) = Parity(n % 2)
 
