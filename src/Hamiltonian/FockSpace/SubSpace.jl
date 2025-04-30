ea44abbc18e38c3b6aab7df13bcf025071ea7078
Ouroboros42 <lucas.gathercole@gmail.com>
2025-04-30 13:53:29 +0100

    Add default momentum to untyped eigenspace

--- src/Hamiltonian/FockSpace/SubSpace.jl
+++ src/Hamiltonian/FockSpace/SubSpace.jl
@@ -11,7 +11,7 @@ end
 EigenSpace{K, N}(momentum, x_symmetrisation::XP, n_parity::NP = nothing) where {K, N, XP, NP} = EigenSpace{K, N, XP, NP}(convert(K, momentum), x_symmetrisation, n_parity)
 EigenSpace{K, N}(momentum = 0; x_symmetrisation = nothing, n_parity = nothing) where {K, N} = EigenSpace{K, N}(momentum, x_symmetrisation, n_parity)
 
-EigenSpace(momentum::K, args...; kwargs...) where K = EigenSpace{signed(K), unsigned(K)}(momentum, args...; kwargs...)
+EigenSpace(momentum::K = 0, args...; kwargs...) where K = EigenSpace{signed(K), unsigned(K)}(momentum, args...; kwargs...)
 
 print(io::IO, eigenspace::EigenSpace) = print(io, "K=$(eigenspace.momentum) X-$(parity_string(eigenspace.x_symmetrisation)) N-$(parity_string(eigenspace.n_parity))")
 show(io::IO, mime::MIME"text/plain", eigenspace::EigenSpace) = print(io, "$(typeof(eigenspace)): $(eigenspace)")
