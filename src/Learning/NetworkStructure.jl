2f14a7fe08c719afe51dbd0812a98fc31e6ffc04
Ouroboros42 <lucas.gathercole@gmail.com>
2025-05-14 21:08:05 +0100

    Allow no hidden layers

--- src/Learning/NetworkStructure.jl
+++ src/Learning/NetworkStructure.jl
@@ -17,11 +17,14 @@ end
 function state_eating_net(output_dims, state_layer_dims, hidden_layer_dims; context_dims = 3, activation = tanh, out_activation = logistic)
     state_encoding_dims = state_layer_dims[end]
     
+    processing_input_dims = context_dims + state_encoding_dims
+    processing_output_dims = isempty(hidden_layer_dims) ? processing_input_dims : hidden_layer_dims[end]
+
     @compact(
         initial_encoding = zeros(state_encoding_dims),
         state_encoding_layers = dense_layers((2 + context_dims + state_encoding_dims, state_layer_dims...), activation),
-        processing_layers = dense_layers((context_dims + state_encoding_dims, hidden_layer_dims...), activation),
-        last_layer = Dense(hidden_layer_dims[end], output_dims, out_activation)
+        processing_layers = dense_layers((processing_input_dims, hidden_layer_dims...), activation),
+        last_layer = Dense(processing_output_dims, output_dims, out_activation)
     ) do (context, states)
         output = stack(map(states) do state
             encoding = initial_encoding
